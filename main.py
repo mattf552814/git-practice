1d9ae1066ae7222b599c0e774e32b5ffd9fbc9bd
Matt Fellenz <mattf53190@gmail.com>
2020-10-15 11:46:11 -0700

    Draw rectangle with given numbers and width and heigh

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ b = int(input('Second number: '))
 import turtle
 painter = turtle.Turtle()
 
+for i in range(2):
+	painter.forward(a)
+	painter.right(90)
+	painter.forward(b)
+	painter.right(90)
+
 wn = turtle.Screen()
 wn.mainloop()
